f5d55f31e1b76853074b991d3f5f5d2ee2c24cef
Lev Vereshchagin <mail@vrslev.com>
2022-11-13 17:50:07 +0300

    Use published js package

--- frontend/core/utils.ts
+++ frontend/core/utils.ts
@@ -1,5 +1,5 @@
 import type { ChordsSection, Song, SongChords } from "@/client";
-import { tranposeChart } from "chord-chart";
+import { tranposeChart } from "chord-chart-wasm";
 
 export function getSongTitle(song: Song): string {
   return song.artist ? `${song.name} — ${song.artist}` : song.name;
--- frontend/core/validation.ts
+++ frontend/core/validation.ts
@@ -16,7 +16,7 @@ import {
   type TestContext,
 } from "yup";
 
-import initChordChart, { validateChart } from "chord-chart";
+import initChordChart, { validateChart } from "chord-chart-wasm";
 
 function passEmptyObjectAsUndefined(value: { [K: string]: unknown }) {
   return Object.values(value).every((el) => !el) ? undefined : value;
